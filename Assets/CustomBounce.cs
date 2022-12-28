52f880f0185365e9f838f90bf3e173bd422c124b
lanhtruonggiang-k63a5 <72454159+lanhtruonggiang-k63a5@users.noreply.github.com>
2022-12-28 16:45:56 +0700

    first time

---
+++ Assets/CustomBounce.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBounce : MonoBehaviour
+{
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+}
